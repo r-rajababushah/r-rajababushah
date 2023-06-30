9d61a2fe266527a24f70e24ae8da029cc2c7499c
Rajababu Shah <rajababushah.in@gmail.com>
2023-06-30 21:46:08 +0530

    minor edit

--- src/components/LastSection.jsx
+++ src/components/LastSection.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 import "./LastSection.scss"
 
 const LastSection = () => {
-  return (
-    <div className='LastSection'>LastSection</div>
-  )
+	return (
+		<div className='LastSection'>
+			&copy; Rajababu Shah , lifetime ownership
+		</div>
+	)
 }
 
 export default LastSection
