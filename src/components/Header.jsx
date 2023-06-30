8e9368a18c4d060bbf3ee115195bcfdd23eb874e
Rajababu Shah <rajababushah.in@gmail.com>
2023-06-30 21:48:06 +0530

    minor edit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,9 +10,7 @@ function Header() {
                     <li><a href="">HOME</a></li>
                     <li><a href="">SKILLS</a></li>
                     <li><a href="">EDUCATION</a></li>
-                    <li><a href="">EXPERIENCE</a></li>
                     <li><a href="">PORTFOLIO</a></li>
-                    <li><a href="">BLOG</a></li>
                     <li><a href="">CONTACT</a></li>
                 </ul>
             </nav>
