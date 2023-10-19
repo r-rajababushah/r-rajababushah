f5b02f6beb4812a1de9026b81e18b48eaacd5cb9
Rajababu Shah <rajababushah.in@gmail.com>
2023-10-19 14:10:16 +0530

    Projects adjusted

--- src/pages/Portfolio/Portfolio.jsx
+++ src/pages/Portfolio/Portfolio.jsx
@@ -10,28 +10,32 @@ function Portfolio() {
         <div className='portfolio'>
             <div className="projects-cards">
                 <div className="project-card">
-                    <div className="project-img">
-                        <img src={imgURL} alt="" />
+                    <div className="project-img" style={{ backgroundImage: "url('/static/marketing_page.png')" }}>
                     </div>
-                    <div className="project-description">ImgDESC</div>
+                    <div className="project-description">
+                        &lt; <Link to={'https://marketing-website-landing-page.vercel.app/'} target='_blank'>Marketing Landing Page</Link> /&gt;
+                    </div>
                 </div>
                 <div className="project-card">
-                    <div className="project-img">
-                        <img src={imgURL} alt="" />
+                    <div className="project-img" style={{ backgroundImage: "url('/static/signupin.png')" }}>
                     </div>
-                    <div className="project-description">ImgDESC</div>
+                    <div className="project-description">
+                        &lt; <Link to={'https://loginform-inreactjs.vercel.app/'} target='_blank'>SignUp and SignIn</Link> /&gt;
+                    </div>
                 </div>
                 <div className="project-card">
-                    <div className="project-img">
-                        <img src={imgURL} alt="" />
+                    <div className="project-img" style={{ backgroundImage: "url('/static/todo-app.png')" }}>
                     </div>
-                    <div className="project-description">ImgDESC</div>
+                    <div className="project-description">
+                        &lt; <Link to={'https://r-react-todo.vercel.app/'} target='_blank'>Todo App</Link> /&gt;
+                    </div>
                 </div>
                 <div className="project-card">
-                    <div className="project-img">
-                        <img src={imgURL} alt="" />
+                    <div className="project-img" style={{ backgroundImage: "url('/static/quote-app.png')" }}>
                     </div>
-                    <div className="project-description">ImgDESC</div>
+                    <div className="project-description">
+                        &lt; <Link to={'https://r-rajababushah.github.io/Random-Quote-Machine/'} target='_blank'>Random Quote Machine</Link> /&gt;
+                    </div>
                 </div>
             </div>
             <div className="project-more-button">
