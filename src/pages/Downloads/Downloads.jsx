7281ca365798c6b397427f82e9fc9f4bdbaebb27
Rajababu Shah <rajababushah.in@gmail.com>
2024-03-09 15:22:24 +0545

    Downloads links styled

--- src/pages/Downloads/Downloads.jsx
+++ src/pages/Downloads/Downloads.jsx
@@ -4,7 +4,21 @@ import "./Downloads.scss";
 function Downloads() {
     return (
         <div className='downloads'>
-            <a href="./VSDC.txt" className="vsdc-codes-download-link" download>Dwonload VSDC free activation Codes</a>
+            <table className="download-links">
+                <thead>
+                    <tr>
+                        <th>Products/Softwares</th>
+                        <th>Download Links</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Video Editor Activation Code:</td>
+                        <td><a href="/Files/VSDC.txt" className="vsdc-codes-download-link" download>Dwonload VSDC free activation Codes</a></td>
+                    </tr>
+                </tbody>
+            </table>
+            
         </div>
     )
 }
