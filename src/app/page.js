233aef0f8ebda6ed9227ea2ba630f8ac15ebdb9f
Rajababu Shah <rajababushah.in@gmail.com>
2025-10-20 18:31:22 +0530

    Update page.js for fun on diwali ligjts

--- src/app/page.js
+++ src/app/page.js
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <div>
         <Header />
+        This is header page , updated on diwali nights of raxaul junction something that else people struggle to do, i am coding in nights of the diwali 🎇 🎇 🎇 🎇 🎇 
     </div>
   );
 }
